bec0c3a56f93409e99462574bf379333dd495b1b
williamwangwei <2840769859@qq.com>
2017-02-28 07:40:16 +0800

    整合SSM

---
+++ src/main/java/com/wwang/controller/ItemController.java
@@ -0,0 +1,22 @@
+package com.wwang.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wwang.pojo.TbItem;
+import com.wwang.service.ItemService;
+
+public class ItemController {
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	public TbItem getItemById(@PathVariable Long itemId) {
+		TbItem tbItem = itemService.getItemById(itemId);
+		return tbItem;
+	}
+
+}
