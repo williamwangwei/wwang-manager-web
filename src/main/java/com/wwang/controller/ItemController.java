844b74ec8a32c76bceab59cca057184a4b17982c
williamwangwei <2840769859@qq.com>
2017-03-02 07:35:05 +0800

    继续开发中

--- src/main/java/com/wwang/controller/ItemController.java
+++ src/main/java/com/wwang/controller/ItemController.java
@@ -1,6 +1,7 @@
 package com.wwang.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,13 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.wwang.pojo.TbItem;
 import com.wwang.service.ItemService;
 
+@Controller
 public class ItemController {
 	@Autowired
 	private ItemService itemService;
 	
 	@RequestMapping("/item/{itemId}")
 	@ResponseBody
-	public TbItem getItemById(@PathVariable Long itemId) {
+	public TbItem getItemById(@PathVariable(value="itemId") Long itemId) {
 		TbItem tbItem = itemService.getItemById(itemId);
 		return tbItem;
 	}
